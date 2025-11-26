4cacc76d3962850bd2b18166a69d781c547b8fb3
racer404 <2238976035@qq.com>
2025-11-26 23:43:24 +0900

    training inf test E1

--- sfmTrain.py
+++ sfmTrain.py
@@ -1,96 +1,116 @@
 import os
+import random
 from typing import List, Any
 
 import cv2
-import numpy as np
+import numpy
 import torch
-from PIL import Image
+from matplotlib import pyplot as plt
+from pytorch_msssim import SSIM
 from torch import optim
 
 import utils
 from learnablePerlin3D import PerlinNoise3D
-from matplotlib import pyplot as plt
+
 
 def train(
         perlin: PerlinNoise3D = None,
-        cameras: [utils.Camera] = None,
+        cameras: List[utils.Camera] = None,
         iterations: int = None,
         lr: float = None,
         ifVisualize: bool = False,
-        ifSaveResult: bool = False,
-        resultTensorPth: str = "./trained_default.pth") -> List[Any]:
-    perlin.cornerVecs.requires_grad = True
+        ifSaveGif: bool = False,
+        resultFolder: str = "results",
+        device: str = "cuda") -> List[Any]:
+
+    os.makedirs(resultFolder, exist_ok=True)
+
+    perlin.cornerVecs.requires_grad_(True)
     optimizer = optim.Adam(
         [perlin.cornerVecs], lr
     )
+
     mse_loss = torch.nn.MSELoss()
     mae_loss = torch.nn.L1Loss()
+    bceLogit_loss = torch.nn.BCEWithLogitsLoss()
+    ssim_loss = SSIM(win_size=11, win_sigma=1.5, data_range=1., size_average=True, channel=1)
 
     frames = []
-    perlin.loss = []
-    dSteps = 100
-    dAlpha = utils.smoothStepsFunc(dSteps).to(device=cams[0].device)
+    totalLoss = []
+    dSteps = 10
+    dAlpha = utils.smoothStepsFunc(dSteps).to(device=device)
 
     for iter in range(iterations):
+        random.shuffle(cams)
         for cam in cameras:
-            dClose, dFar = cam.getDepthRange(perlin.center, perlin.scale)
-            samplePoints_Volume, validPoints = cam.sampleVolumeBySteps(dClose, dFar, dSteps)
-            renderedPoints_Volume, output_mask_Volume = perlin.getValue(samplePoints_Volume, validPoints)
+            p_close = 0.00001
+            p_far = 2.00001
+            samplePoints_Volume = cam.sampleVolumeBySteps(p_close, p_far, dSteps)[0]
+            renderedPoints_Volume = perlin.getValue(samplePoints_Volume) / 2. + 0.5
 
             renderedPoints_Flat = renderedPoints_Volume.reshape(cam.width * cam.height, dSteps)
             renderedPoints = renderedPoints_Flat @ dAlpha
 
-            gtImage = (torch.tensor(cv2.imread(cam.image,cv2.IMREAD_GRAYSCALE), dtype=torch.float64, device="cuda")/255.).T
-            gtImage_Flat = gtImage.flatten()
+            pred_img = renderedPoints.reshape(cam.width, cam.height)
 
-            output_mask_Flat = output_mask_Volume.reshape(cam.width * cam.height, dSteps)
-            output_mask = torch.any(output_mask_Flat, dim=1)
+            gtImage = (torch.tensor(cv2.imread(cam.image,cv2.IMREAD_GRAYSCALE), dtype=torch.float64, device=device)/255.).T
 
-            loss = mse_loss(renderedPoints[output_mask], gtImage_Flat[output_mask])
+            loss_ssim = 1 - ssim_loss(pred_img.unsqueeze(0).unsqueeze(0).permute(0,1,3,2), gtImage.unsqueeze(0).unsqueeze(0).permute(0,1,3,2))
+            loss_mse = mse_loss(pred_img, gtImage)
+            loss = 0.1 * loss_ssim + 0.9 * loss_mse
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             print(f"Iteration {iter + 1}/{iterations}, Loss: {loss.item()}")
-            perlin.loss.append(loss.item())
+            totalLoss.append(loss.item())
 
             if ifVisualize:
                 # renderedPoints[~output_mask] = ? #Background
-                output_img = renderedPoints.reshape(cam.width, cam.height)
                 torch.cuda.synchronize()
-                showImg = output_img.T.cpu().detach().numpy()
+                showImg = pred_img.T.cpu().detach().numpy()
                 showGt = gtImage.T.cpu().detach().numpy()
                 cv2.imshow("Training", showImg)
                 cv2.imshow("GT", showGt)
                 cv2.waitKey(1)
 
-                if ifSaveResult:
-                    frames.append(showImg)
+        if ifSaveGif:
+            print("UNDER CONSTRUCTION")
 
-    if ifSaveResult:
-        frames = [Image.fromarray(frame) for frame in frames]
-        out_dir = os.path.join(os.getcwd(), "results")
-        os.makedirs(out_dir, exist_ok=True)
-        frames[0].save(
-            f"{out_dir}/training.gif",
-            save_all=True,
-            append_images=frames[1:],
-            optimize=False,
-            duration=1,
-            loop=0,
-        )
+    if ifSaveGif:
+        print("UNDER CONSTRUCTION")
 
-    perlin.cornerVecs.requires_grad = False
-    perlin.writeTensor(resultTensorPth)
-    return perlin.loss
+    perlin.cornerVecs.requires_grad_(False)
+    perlin.writeTensor(f"{resultFolder}/0.pth")
+
+    return totalLoss
 
 if __name__ == "__main__":
     dataset = "kitchen"
+    trainingSetup = "test_INF"
+    outputFolder = f"{dataset}/trained/{trainingSetup}"
+    sceneCenter = torch.tensor([-0.461083, 1.5, 1.5], dtype=torch.float64, device="cuda")
+
     cams = utils.readColmapSceneInfo(dataset)
-    testCenter = torch.tensor([-0.461083, 1.5, 1.5], dtype=torch.float64, device="cuda")
+    perlin = PerlinNoise3D(res=2, center=sceneCenter, device="cuda")
 
-    perlin = PerlinNoise3D(scale=2, res=30, center=testCenter, device="cuda")
+    loss = train(perlin, cams, 100, 0.01, True, False, outputFolder)
 
-    loss = train(perlin, cams, 100, 0.01, False, False,"kitchen/trained.pth")
+    loss_arr = numpy.array(loss)
+    loss_arr = loss_arr.reshape([-1,len(cams)])
+    loss_per_batch = loss_arr.mean(axis=1)
     torch.cuda.synchronize()
-    plt.plot(loss)
-    plt.show()
+    ## END OF TRAINING
+
+
+
+    # === Save histograms and plots ===
+    def save_plot(fig_name):
+        """Helper function to save current matplotlib figure."""
+        plt.savefig(os.path.join(outputFolder, fig_name))
+        plt.close()
+
+    plt.figure()
+    plt.plot(loss_per_batch)
+    plt.annotate(str(loss_per_batch[-1]), xy=(len(loss_per_batch) - 1, loss_per_batch[-1]))
+    save_plot("loss_batch.png")
